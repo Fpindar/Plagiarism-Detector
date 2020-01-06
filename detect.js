edb4e361a19dcb31cd9db6abb2033c553fb0a7ca
Fpindar <32140475+Fpindar@users.noreply.github.com>
2020-01-06 13:33:23 -0500

    Delete detect.js

--- detect.js
+++
@@ -1,8 +0,0 @@
-const getJaccardSimilarityScore = require("../algorithms/jaccard/");
-
-module.exports = {
-  // Get jaccard similarity score of 2 files
-  async getPlagarismScore(source, input) {
-    return getJaccardSimilarityScore(source, input);
-  }
-};
